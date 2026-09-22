f0cecda6ea9c432585ed10fe4f1d0489bf55a489
Slimfingers-com <github@eckle.net>
2026-09-22 16:50:40 +0200

    Export source metadata models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -14,6 +14,7 @@ from app.models.feed import Feed
 from app.models.perspective import ArticlePerspective
 from app.models.search_document import SearchDocument
 from app.models.source import Source
+from app.models.source_metadata import SourceClassification, SourceReachMetric
 from app.models.story import Story, StoryArticle
 from app.models.story_processing import StoryProcessingRun, StoryProcessingState
 from app.models.topic import ArticleTopic, Topic
@@ -27,6 +28,8 @@ __all__ = [
     "Feed",
     "SearchDocument",
     "Source",
+    "SourceClassification",
+    "SourceReachMetric",
     "Entity",
     "EntityAlias",
     "ArticleEntity",
